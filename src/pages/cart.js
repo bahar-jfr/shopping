b7578641a919a9d9b8e5265c9ac979b952f6c84b
alirezaamb <ababaei78@gmail.com>
2024-03-28 16:40:05 +0330

    feat:implement cart page

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,24 +1,47 @@
-import { El } from "../components/shared/El";
-import { Router } from "../router";
-import { Main_Route } from "../router/routes";
+import { getOrders } from '../api/getData';
+import { BasketCard } from '../components/basketCard';
+import { Button } from '../components/kit/button';
+import { El } from '../components/shared/El';
+import { Router } from '../router';
+import { Main_Route } from '../router/routes';
 
 export const Cart = () => {
-  return El({
-    element: "p",
-    innerText: "cart page",
-    children: [
-      El({
-        element: "button",
-        innerText: "back to home",
-        eventListener: [
-          {
-            event: "click",
-            callback: () => {
-              Router().navigate(Main_Route);
-            },
-          },
-        ],
-      }),
-    ],
-  });
+  getOrders()
+    .then((data) => {
+      renderCart(data);
+    })
+    .catch(() => {
+      const container = document.querySelector('#cartContainer');
+      container.innerHTML = 'something went wrong';
+    });
+
+  const renderCart = (data) => {
+    const container = document.querySelector('#cartContainer');
+    container.innerHTML = '';
+
+    const cartDiv = El({
+      element: 'div',
+      className: 'p-4',
+      children: [
+        El({
+          element: 'div',
+          className: 'grid grid-cols-3 gap-6 ',
+          children: data.map((item) => BasketCard(item)),
+        }),
+        Button({
+          text: 'Home',
+          className: 'bg-green-500 w-24',
+          onClick: navigateToHomeHandler,
+        }),
+      ],
+    });
+
+    container.appendChild(cartDiv);
+  };
+
+  const navigateToHomeHandler = () => {
+    Router().navigate(Main_Route);
+  };
+
+  return El({ element: 'div', id: 'cartContainer', innerText: 'Loading...' });
 };
